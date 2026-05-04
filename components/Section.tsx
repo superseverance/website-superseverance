c1e7397abcd792176a739641634b7732c5ab9023
mircocotugno <mirco.cotugno@gmail.com>
2026-05-04 16:00:23 +0200

    design Section

--- components/Section.tsx
+++ components/Section.tsx
@@ -1,4 +1,5 @@
 import type { Section as SectionType } from "@/sbComponentType"
+import { createElement } from "react"
 import Markdown from "markdown-to-jsx/react"
 import { tv } from "tailwind-variants"
 
@@ -13,23 +14,60 @@ export interface SectionComponent {
 }
 
 const classes = tv({
-  slots: {},
+  slots: {
+    section: "",
+    container: "",
+    header: "",
+    columns: ""
+  },
   variants: {}
 })
 
 export function Section({ blok }: SectionComponent) {
   const { heading, body, theme, justify } = blok
-  // const { } = classes()
+  const { section, container, header, columns } = classes()
   return (
-    <div className="" {...storyblokEditable(blok)}>
-      <div className="">
-        <Markdown>
-          {heading}
-        </Markdown>
-      </div>
-      <div className="">
-        {body?.map((item) => <StoryblokComponent blok={item} key={item._uid} />)}
+    <div className={section()} {...storyblokEditable(blok)}>
+      <div className={container()}>
+        <div className={header()}>
+          <Markdown>
+            {heading}
+          </Markdown>
+        </div>
+        <div className={columns()}>
+          {body?.map((item) => <StoryblokComponent blok={item} key={item._uid} />)}
+        </div>
       </div>
     </div>
   )
+}
+
+const styles = tv({
+  slots: {
+    h1: "",
+    h2: "",
+    h3: "",
+    p: "",
+  }
+})
+
+const Tags = [
+  "h1",
+  "h2",
+  "h3",
+  "p",
+] as const;
+
+function Typography() {
+  const _styles = styles();
+  return Object.fromEntries(
+    Tags.map((tag) => {
+      const className = _styles[tag]();
+      return [
+        tag,
+        ({ children }: { children: string }) =>
+          createElement(tag, { className }, children),
+      ];
+    })
+  );
 }
