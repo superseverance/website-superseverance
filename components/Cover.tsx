3ab406d78d88ddad6c1b6076e971ccdca5fbda87
mircocotugno <mirco.cotugno@gmail.com>
2026-05-04 16:08:58 +0200

    design Cover

--- components/Cover.tsx
+++ components/Cover.tsx
@@ -1,5 +1,8 @@
 import type { Cover as CoverType } from "@/sbComponentType"
 import { tv } from "tailwind-variants"
+import getResized from "@/libs/sbImage";
+import { Image as HeroImage } from "@heroui/react"
+import { default as NextImage } from "next/image"
 
 import {
   SbBlokData,
@@ -12,17 +15,33 @@ export interface CoverComponent {
 }
 
 const classes = tv({
-  slots: {},
+  slots: {
+    cover: "",
+    container: "",
+    background: "",
+    image: ""
+  },
   variants: {}
 })
 
 export function Cover({ blok }: CoverComponent) {
-  const { body, background, theme, align, height } = blok
-  // const { } = classes()
+  const { body, source, theme, align, height } = blok
+  const { cover, container, background, image } = classes()
   return (
-    <div className="" {...storyblokEditable(blok)}>
-      {body?.map((item) => <StoryblokComponent blok={item} key={item._uid} />)}
-      {background && <div className="background" />}
+    <div className={cover()} {...storyblokEditable(blok)}>
+      <div className={container()}>
+        {body?.map((item) => <StoryblokComponent blok={item} key={item._uid} />)}
+        {source?.filename && (
+          <HeroImage
+            src={getResized({ filename: source.filename, focus: source.focus })}
+            classNames={{ wrapper: background(), img: image() }}
+            alt={source.alt || ""}
+            radius="none"
+            as={NextImage}
+            loading="eager"
+            fill />
+        )}
+      </div>
     </div>
   )
 }
